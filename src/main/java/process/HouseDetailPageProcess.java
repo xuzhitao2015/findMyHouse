44f22faef2b219bd59483de53e4e235d9a2bd0c3
zliang <zliang3110@gmail.com>
2016-04-22 17:12:45 +0800

    加入webmagic框架

--- src/main/java/process/HouseDetailPageProcess.java
+++ src/main/java/process/HouseDetailPageProcess.java
@@ -11,10 +11,29 @@ import us.codecraft.webmagic.processor.PageProcessor;
  */
 public class HouseDetailPageProcess implements PageProcessor {
     private Site site = Site.me().setRetryTimes(3).setSleepTime(1000).setCharset("utf-8").setTimeOut(1000*10);
-
+    private static String URL_LIST = "http://zz\\.58\\.com/zufang/0/(pn[0-9]+/.*)?";
+    private static String URL_INFO = "http://zz\\.58\\.com/zufang/.+\\.shtml\\?version=.*&psid=.*&entinfo=.*";
     public void process(Page page) {
-            System.out.println(page.getUrl());
-            page.putField("detailPage",page.getHtml().$(".tbimg a.t").regex("\"http://zz\\.58\\.com/.*?\"").all());
+            if(page.getUrl().regex(URL_INFO).match()){
+                //获取房屋价格
+                page.putField("price",page.getHtml().$("em.house-price").regex("\\d+"));
+                if (page.getResultItems().get("price") == null) {
+                    //skip this page
+                    page.setSkip(true);
+                }
+                System.out.println(page.getHtml().$("div.fl.house-type.c70").regex("[1-9](?=\\u5ba4)").toString());
+                //获取户型
+                page.putField("sansh",page.getHtml().$("div.fl.house-type.c70").regex("[1-9](?=\\u5ba4)").toString());
+                //获取区域
+                page.putField("area",page.getHtml().regex(".fl.xiaoqu.c70 a:nth-child(1)").toString());
+            }else if(page.getUrl().regex(URL_LIST).match()){
+                page.addTargetRequests(page.getHtml().$(".tbimg a.t").links().regex("http://zz\\.58\\.com/.*").all());
+                page.addTargetRequests(page.getHtml().$("a.next").links().all());
+            }else {
+                System.out.println("不支持的链接格式："+page.getUrl());
+            }
+
+
     }
 
     public Site getSite() {
@@ -24,7 +43,7 @@ public class HouseDetailPageProcess implements PageProcessor {
     public static void main(String[] args) {
         Spider.create(new HouseDetailPageProcess())
                 //从"https://github.com/code4craft"开始抓
-                .addUrl("http://zz.58.com/zufang/0/pn2/?PGTID=0d300008-0015-6cbd-9def-d4134f1d8258&ClickID=2")
+                .addUrl("http://zz.58.com/zufang/0/")
                 //开启5个线程抓取
                 .thread(4)
                 //打印输出
